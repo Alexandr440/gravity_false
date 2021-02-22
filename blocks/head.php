c5c4ef8f18790e1e0d05e3dc0ca43a9439bf831d
Alexandr440 <44962988+Alexandr440@users.noreply.github.com>
2021-02-22 11:38:37 +0500

    ооо

---
+++ blocks/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>monsr_page</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="fontawesom/css/font-awesome.min.css">
+
+    <link href="./css/all.min.css">
+    <link rel="stylesheet" href="./slick/slick.css">
+    <link rel="stylesheet" href="./slick/slick-theme.css">
+</head>
--- js/main.js
+++ js/main.js
@@ -1,72 +1,58 @@
-$(document).ready(function(){
-    
-          
-    //slider
-    $('.sl1').slick({
-        infinite: true,
-        slidesToShow: 1 ,
-        slidesToScroll: 1,
-        //autoplay: true,
-        //autoplaySpeed: 1000,
-        /*lazyLoad: 'ondemand',*/
-        //speed: 500,
-        //loop: true,
-        arrows: true,
-        prevArrow: '<div class="prev"><i class ="fa fa-caret-left fa-2x"></div>',
-        nextArrow: '<div class="next"><i class ="fa fa-caret-right fa-2x"></div>',
-        dots: false,
-        fade: true,
-        asNavFor: '.sl2'      
+//"use strict";
+document.addEventListener("DOMContentLoaded", function(){
+   //открываем и закрываем отзывы
+  const revBtn = document.querySelector('.reviews__btn');
+  console.log(revBtn);
 
+  const reviewsCards = document.querySelector('.reviews__cards');
 
-    });
+  revBtn.onclick = function() {   
+    reviewsCards.classList.toggle('open-cards');
+    if (revBtn.innerText.toLowerCase() === 'показать') {
+      revBtn.innerText = "скрыть";
+    } else {
+      revBtn.innerText = 'показать';
+    }
+  };
 
-    $('.sl2').slick({
-        centerMode: true,
-        centerPadding: '50px', 
-        infinite: true,      
-        slidesToShow: 3 ,
-        autoplay: false,
-        dots: false, 
-        asNavFor: '.sl1',
-        focusOnSelect: true,
-        arrows: false,
-        
-               
-
-    });
-
-
-    
-    //замедление прокрутки из меню
-
-    $('.menu a ').on('click', function(e){
-		e.preventDefault();
-
-		//прикручиваем актив класс
-		$('.menu a').removeClass("active").filter(this).addClass("active");
-
-
-			//прокрутка 
-		var selector = $(this).attr('href');
-		
-			
-		var h = $(selector);		
-
-		$('html,body').animate({
-			scrollTop: h.offset().top-70    
-		}, 1000);
-
-    });
-
-    
-
-
-
+  //slider
+  $('.sl1').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,    
+    arrows: true,
+    prevArrow: '<div class="prev"><i class ="fa fa-caret-left fa-2x"></div>',
+    nextArrow: '<div class="next"><i class ="fa fa-caret-right fa-2x"></div>',
+    dots: false,
+    fade: true,
+    asNavFor: '.sl2'
   });
 
+  $('.sl2').slick({
+    centerMode: true,
+    centerPadding: '50px',
+    infinite: true,
+    slidesToShow: 3,
+    autoplay: false,
+    dots: false,
+    asNavFor: '.sl1',
+    focusOnSelect: true,
+    arrows: false
+  });
 
+  //замедление прокрутки из меню  
+  $('.menu a').on('click', function (e) {
+    e.preventDefault();
+   
+    $('.menu a').removeClass("active").filter(this).addClass("active");
+ 
+    var selector = $(this).attr('href');
+    var h = $(selector);
+    $('html,body').animate({
+      scrollTop: h.offset().top - 70
+    }, 1000);
 
-
-
-
+  });
+ const catalogButtons = document.querySelector('.catalog__buttons');
+  console.log(catalogButtons);
+});
